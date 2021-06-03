3e037b6785e47ff8fb84f35deec681f1669896f2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 18:19:50 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I went to the theatre last night. I met Sue ..... the interval.",
+        options:["during", "for"],
+        answer: 0
+    },
+    {
+        q:"I felt really ill last week. I could hardly eat anything ..... three days.",
+        options:["during", "for" ],
+        answer: 1
+    },
+    {
+        q:"The traffic was bad. We were stuck in a traffic jam ..... two hours.",
+        options:["for", "during"],
+        answer: 0
+    },
+    {
+        q:"Production at the factory was seriously affected ..... the strike.",
+        options:["for", "during"],
+        answer: 1
+    },
+    {
+     q:"Sarah was very angry with me. She didn’t speak to me ..... a week.",
+        options:["for", "during"],
+        answer: 0   
+    },
+    {
+        q:"I don’t have much free time ..... the week, but I relax at weekends.",
+        options:["for", "during"],
+        answer: 1
+    },
+    {
+        q:"I need a break. I think I’ll go away ..... a few days.",
+        options:["for", "during"],
+        answer: 0
+    },
+    {
+        q:"The president gave a short speech. She spoke ..... only ten minutes.",
+        options:["during", "for"],
+        answer: 1
+    },
+    {
+        q:"We were hungry when we arrived. We hadn’t eaten anything ..... the journey.",
+        options:["during", "during"],
+        answer: 0
+    },
+    {
+        q:"We were hungry when we arrived. We hadn’t eaten anything ..... eight hours.",
+        options:["during", "for"],
+        answer: 1
+    }
+
+]
